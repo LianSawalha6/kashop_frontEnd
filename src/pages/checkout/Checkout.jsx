edd868aeeead385079c61f3611f73728151f80d7
LianSx6 <s12323588@stu.najah.edu>
2026-08-05 22:22:37 +0300

    designed the checkout page

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import useCart from '../../hooks/useCart'
 import useCheckout from '../../hooks/useCheckout'
-import { Box, Button, CircularProgress, FormControl, IconButton, InputLabel, MenuItem, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import { Box, Button, Card, CardContent, CircularProgress, Divider, FormControl, IconButton, InputLabel, MenuItem, Paper, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import RemoveIcon from '@mui/icons-material/Remove';
 
 const Checkout = () => {
@@ -11,10 +11,42 @@ const Checkout = () => {
   const {mutate:checkout,isPending}=useCheckout()
   if(isError) return <Box >{error.message}</Box>
   if(isLoading)return <CircularProgress/>
+
+  const subtotalCost = data.items.reduce((sum, item) => sum +item.totalPrice, 0);
+  console.log(subtotalCost)
   
   return (
-    <Box>
-    <TableContainer>
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: {
+          xs: "column",
+          md: "row",
+        },
+        alignItems: {
+          xs: "stretch",
+          md: "center",
+        },
+        justifyContent: "center",
+        gap: 4,
+        p: {
+          xs: 2,
+          sm: 3,
+          md: 5,
+        },
+      }}
+    >
+      <TableContainer
+        component={Paper}
+        sx={{
+          flex: 2,
+          borderRadius: 4,
+          border: "1px solid",
+          borderColor: "divider",
+          boxShadow: 2,
+        }}
+      >
         <Table>
           <TableHead>
             <TableCell>Product name</TableCell>
@@ -23,42 +55,112 @@ const Checkout = () => {
             <TableCell>Total</TableCell>
           </TableHead>
 
-
           <TableBody>
-            {data.items.map((item)=>(
-            <TableRow key={item.id}>
-              <TableCell>{item.productName}</TableCell>
-              <TableCell>{item.price}$</TableCell>
-              <TableCell>
-                <Box sx={{display:'flex',alignItems:'center'}}>
-                  
-                  <Typography>{item.count}</Typography>
-                  
-                </Box>
-              </TableCell>
-              <TableCell>{item.totalPrice}$</TableCell>
-            </TableRow>
+            {data.items.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.productName}</TableCell>
+                <TableCell>{item.price}$</TableCell>
+                <TableCell>
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <Typography>{item.count}</Typography>
+                  </Box>
+                </TableCell>
+                <TableCell>{item.totalPrice}$</TableCell>
+              </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-    
-    <FormControl fullWidth>
-  <InputLabel id="demo-simple-select-label">Payment Method</InputLabel>
-  <Select
-    labelId="demo-simple-select-label"
-    id="demo-simple-select"
-    value={paymentMethod}
-    label="Payment"
-    onChange={(e)=>{setPaymentMethod(e.target.value)}}
-  >
-    <MenuItem value={"Cash"}>Cash</MenuItem>
-    <MenuItem value={'Visa'}>Visa</MenuItem>
-  </Select>
-</FormControl>
-<Button variant='contained'onClick={()=>checkout({paymentMethod})}>Pay now</Button>
+
+      <Card
+        sx={{
+          flex: 1,
+          border: "1px solid",
+          borderColor: "divider",
+          boxShadow: 2,
+          borderRadius: 4,
+        }}
+      >
+        <CardContent>
+          <Typography
+            variant="h6"
+            sx={{
+              mb: 3,
+            }}
+          >
+            Order Summary
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              mb: 3,
+            }}
+          >
+            <Typography>Subtotal ({data.items.length} items)</Typography>
+            <Typography>{subtotalCost}$</Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              mb: 3,
+            }}
+          >
+            <Typography>shipping</Typography>
+            <Typography>Free</Typography>
+          </Box>
+          <Divider sx={{ my: 3 }} />
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              mb: 3,
+            }}
+          >
+            <Typography fontWeight={700}>Total</Typography>
+
+            <Typography variant="h5" fontWeight={700}>
+              ${subtotalCost}
+            </Typography>
+          </Box>
+
+          <FormControl fullWidth sx={{ mb: 3 }}>
+            <InputLabel id="demo-simple-select-label">
+              Payment Method
+            </InputLabel>
+            <Select
+              sx={{
+                mb: 3,
+              }}
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={paymentMethod}
+              label="Payment"
+              onChange={(e) => {
+                setPaymentMethod(e.target.value);
+              }}
+            >
+              <MenuItem value={"Cash"}>Cash</MenuItem>
+              <MenuItem value={"Visa"}>Visa</MenuItem>
+            </Select>
+          </FormControl>
+
+          <Button
+            fullWidth
+            variant="contained"
+            onClick={() => checkout({ paymentMethod })}
+            sx={{
+              my: 5,
+            }}
+          >
+            Pay now
+          </Button>
+        </CardContent>
+      </Card>
     </Box>
-  )
+  );
 }
 
 export default Checkout
